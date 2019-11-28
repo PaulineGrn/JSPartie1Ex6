3c620f1a4afb3a7b7b1e5fe8bfdb489b57d61c2d
PaulineGrn <pauline.gernigon@orange.fr>
2019-11-28 16:58:34 +0100

    JSPartie1Ex6

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function validation(){
+  var size=document.getElementById("size").value;
+  var dateOfBirth=document.getElementById("dateOfBirth").value;
+  var result=(size*2+5)*50
+  alert(result);
+}
