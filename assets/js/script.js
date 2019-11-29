92339a2d14fcefeea36bae120693d1a088d93f6e
PaulineGrn <pauline.gernigon@orange.fr>
2019-11-29 11:23:17 +0100

    JSPartie1Ex6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
+//Création d'une fonction
 function validation(){
+  //Création de variables
   var size=document.getElementById("size").value;
   var dateOfBirth=document.getElementById("dateOfBirth").value;
-  var result=(size*2+5)*50
-  alert(result);
+  //variable result avec le calcul de l'exercice
+  if(isNaN(size)==true||isNaN(dateOfBirth)==true){
+    alert("erreur, ce n'est pas un nombre");
+    //Sinon, le résultat du calcul
+  }else{
+    var result=((size*2+5)*50-dateOfBirth+1769);
+    //Boîte de dialogue pour afficher le resultat des différents calculs
+    alert(result);
+  }
 }
